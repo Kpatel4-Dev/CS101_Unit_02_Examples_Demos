bd247919318f56e9d7470c25d7cfc3aa8fd1aa75
Kavita Patel <kpatel@miracosta.edu>
2025-08-03 20:00:21 -0700

    Create Print_f_String_Ex1.py

---
+++ Print_f_String_Ex1.py
@@ -0,0 +1,16 @@
+#printing floating point number  examples
+
+number = 12345.67891213
+
+print(f'{number}') #print the number as it is
+
+print(f'{number:.2f}') #print the number to 2 decimal places
+
+print(f'{number:10.2f}') #print the number to 2 decimal places, 10 spaces wide
+
+print(f'{number:<10.2f}') #print the number to 2 decimal places, 10 spaces wide and left justified
+
+print(f'{number:10,.2f}') #print the number to 2 decimal places, 10 spaces wide and comma seperated
+
+print(f'My number is {number:,.3f}!') #print the number to 3 decimal places, comma seperated
+
